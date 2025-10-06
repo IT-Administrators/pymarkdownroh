fc7270d8529c8c9c4ffd03a3be22e9e6fc3b31db
IT-Administrators <91905626+IT-Administrators@users.noreply.github.com>
2025-10-06 14:29:58 +0200

    Added function wo write bold and italic
    Added a function to write bold an italic at the same time.
    
    **bold**
    *italic*
    ***bolditalic***

--- src/pymarkdownroh/mdformat.py
+++ src/pymarkdownroh/mdformat.py
@@ -12,3 +12,8 @@ def write_bold(string: str):
     """Convert provided string to bold string."""
 
     return _EMPHASIZEPREF + _EMPHASIZEPREF + string + _EMPHASIZEPREF + _EMPHASIZEPREF
+
+def write_bold_italic(string: str):
+    """Convert provided string to italic and bold."""
+
+    return _EMPHASIZEPREF + _EMPHASIZEPREF + _EMPHASIZEPREF + string + _EMPHASIZEPREF + _EMPHASIZEPREF + _EMPHASIZEPREF
