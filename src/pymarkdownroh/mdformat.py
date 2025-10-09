5c9431728044abff23324929ed50418d19ff8e4e
IT-Administrators <91905626+IT-Administrators@users.noreply.github.com>
2025-10-09 11:28:03 +0200

    Added functionality
    Added the functionality to write substrings in
    bold, italic or both by specifying start and end position.
    
    Created testcases as well.

--- src/pymarkdownroh/mdformat.py
+++ src/pymarkdownroh/mdformat.py
@@ -9,17 +9,26 @@ _EMPHASIZEKIND = {
     "boldItalic": f"{_EMPHASIZEPREF + _EMPHASIZEPREF+ _EMPHASIZEPREF}"
 }
 
-def write_bold(string: str):
+def write_bold(string: str, startbold: int = 0, endbold: int = 1000) -> str:
     """Convert provided string to bold string."""
 
-    return _EMPHASIZEKIND["bold"] + string + _EMPHASIZEKIND["bold"]
+    _verify_range(startbold, endbold)
+    return string[:startbold] + _EMPHASIZEKIND["bold"] + string[startbold:endbold] + _EMPHASIZEKIND["bold"] + string[endbold:]
 
-def write_italics(string: str):
+def write_italics(string: str, startitalic:int = 0, enditalic: int = 1000) -> str:
     """Convert provided string to italic string."""
 
-    return _EMPHASIZEKIND["italic"] + string + _EMPHASIZEKIND["italic"]
+    _verify_range(startitalic, enditalic)
+    return string[:startitalic] + _EMPHASIZEKIND["italic"] + string[startitalic:enditalic] + _EMPHASIZEKIND["italic"] + string[enditalic:]
 
-def write_bold_italic(string: str):
+
+def write_bold_italic(string: str, startboldital: int = 0, endboldital: int = 1000) -> str:
     """Convert provided string to italic and bold."""
 
-    return _EMPHASIZEKIND["boldItalic"] + string + _EMPHASIZEKIND["boldItalic"]
+    _verify_range(startboldital, endboldital)
+    return string[:startboldital] + _EMPHASIZEKIND["boldItalic"] + string[startboldital:endboldital] + _EMPHASIZEKIND["boldItalic"] + string[endboldital:]
+
+
+def _verify_range(start: int, end: int) -> Exception:
+    if end < start:
+        raise ValueError(f"End {end} smaller than Start {start}.")
--- tests/test_pymarkdownroh.py
+++ tests/test_pymarkdownroh.py
@@ -9,13 +9,26 @@ from src.pymarkdownroh import *
 importdir()
 
 TESTS = {
+    # Emphasizing tests whole string.
     "EMPHASIZING":["Test"],
     "BOLD_RESULTS":["**Test**"],
     "ITALIC_RESULTS":["*Test*"],
     "BOLDITALIC_RESULTS":["***Test***"],
+    # Emphasizing tests substring.
+    "EMPHASIZING_SUB": ["Python is great."],
+    "BOLD_SUB_RESULTS": ["**Python** is great."],
+    "ITALIC_SUB_RESULTS": ["*Python* is great."],
+    "BOLDITALIC_SUB_RESULTS": ["***Python*** is great."],
 
+    # Headline tests.
     "HEADLINES":["Title", "Lvl2 Headline", "Lvl3 Headline", "Lvl4 Headline", "Lvl5 Headline", "Lvl6 Headline"],
     "HEADLINE_RESULTS":["# Title", "## Lvl2 Headline", "### Lvl3 Headline", "#### Lvl4 Headline", "##### Lvl5 Headline", "###### Lvl6 Headline"]
+}
+
+EXAMPLEFILES = {
+    "EMPHASIZING": "./examples/EMPHASIZE.md",
+    "HEADLINES": "./examples/HEADLINES.md",
+    "TABLES": "./examples/TABLES.md"
 }
 
 class TestPymarkdownroh_Emphasizing(unittest.TestCase):
@@ -36,6 +49,18 @@ class TestPymarkdownroh_Emphasizing(unittest.TestCase):
         for i in range(len(TESTS["EMPHASIZING"])):
             self.assertEqual(write_bold_italic(TESTS["EMPHASIZING"][i]),TESTS["BOLDITALIC_RESULTS"][i])
 
+    def test_bold_emphasizing_substring(self):
+        for i in range(len(TESTS["EMPHASIZING_SUB"])):
+            self.assertEqual(write_bold(TESTS["EMPHASIZING_SUB"][i], startbold=0, endbold=6), TESTS["BOLD_SUB_RESULTS"][i])
+    
+    def test_bold_emphasizing_substring(self):
+        for i in range(len(TESTS["EMPHASIZING_SUB"])):
+            self.assertEqual(write_italics(TESTS["EMPHASIZING_SUB"][i], startitalic=0, enditalic=6), TESTS["ITALIC_SUB_RESULTS"][i])
+
+    def test_bold_emphasizing_substring(self):
+        for i in range(len(TESTS["EMPHASIZING_SUB"])):
+            self.assertEqual(write_bold_italic(TESTS["EMPHASIZING_SUB"][i], startboldital=0, endboldital=6), TESTS["BOLDITALIC_SUB_RESULTS"][i])
+
 class TestPymarkdownroh_Headlines(unittest.TestCase):
     """Test headline and title creation of pymarkdownroh module."""
 
@@ -46,6 +71,10 @@ class TestPymarkdownroh_Headlines(unittest.TestCase):
         for i in range(len(TESTS["HEADLINES"])):
             # Create with i +1 because the lsit starts counting by zero but a headline must have one #.
             self.assertEqual(create_headline(i +1, TESTS["HEADLINES"][i]), TESTS["HEADLINE_RESULTS"][i])
+
+            with open(EXAMPLEFILES["HEADLINES"], "w") as f:
+                for i in range(len(TESTS["HEADLINES"])):
+                    f.write(create_headline(i +1, TESTS["HEADLINES"][i]) + "\n")
     
 
 if __name__ == '__main__':
