89d65bd6c185ed9c3f5e32b4858f7939a3e46fe5
IT-Administrators <91905626+IT-Administrators@users.noreply.github.com>
2025-10-09 10:38:23 +0200

    Added _EMPHASIZEKIND dictionary
    The _EMPHASIZEKIND dictionary stores the different
    strings for emphasizing.
    
    _EMPHASIZEKIND = {
        "bold": "**",
        "italic": "*",
        "boldItalic": "***"
    }

--- src/pymarkdownroh/mdformat.py
+++ src/pymarkdownroh/mdformat.py
@@ -3,17 +3,23 @@
 # Default character used when converting emphasizing string.
 _EMPHASIZEPREF = "*"
 
-def write_italics(string: str):
-    """Convert provided string to italic string."""
-
-    return _EMPHASIZEPREF + string + _EMPHASIZEPREF
+_EMPHASIZEKIND = {
+    "bold": f"{_EMPHASIZEPREF + _EMPHASIZEPREF}",
+    "italic": f"{_EMPHASIZEPREF}",
+    "boldItalic": f"{_EMPHASIZEPREF + _EMPHASIZEPREF+ _EMPHASIZEPREF}"
+}
 
 def write_bold(string: str):
     """Convert provided string to bold string."""
 
-    return _EMPHASIZEPREF + _EMPHASIZEPREF + string + _EMPHASIZEPREF + _EMPHASIZEPREF
+    return _EMPHASIZEKIND["bold"] + string + _EMPHASIZEKIND["bold"]
+
+def write_italics(string: str):
+    """Convert provided string to italic string."""
+
+    return _EMPHASIZEKIND["italic"] + string + _EMPHASIZEKIND["italic"]
 
 def write_bold_italic(string: str):
     """Convert provided string to italic and bold."""
 
-    return _EMPHASIZEPREF + _EMPHASIZEPREF + _EMPHASIZEPREF + string + _EMPHASIZEPREF + _EMPHASIZEPREF + _EMPHASIZEPREF
+    return _EMPHASIZEKIND["boldItalic"] + string + _EMPHASIZEKIND["boldItalic"]
